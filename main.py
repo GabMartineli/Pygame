327f4db958d3199e16595edcc7d2e886f65f4dd9
Gabriel <156025724+GabMartineli@users.noreply.github.com>
2025-09-16 18:57:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ print('Iniciando laço')
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            print('Fechando janela')
+            print('Fechando janelaa')
             pygame.quit() # fechar janela
             quit() # fechar jogo
             
