44d4c9a3324dbccea8b727267910db2e7a780e51
Gabriel <gabrielmartineli88@Gmail.com>
2025-09-16 22:49:23 -0300

    create menu title import assets added background image and sound on menu

--- codegame/game.py
+++ codegame/game.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from codegame.const import WIN_HEIGHT, WIN_WIDTH
 from codegame.menu import Menu
 import pygame
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size = (700, 520))
+        self.window = pygame.display.set_mode(size = (WIN_WIDTH, WIN_HEIGHT))
 
-    def run(self,):
+    def run(self):
         
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
             
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    print('Fechando janela')
-                    pygame.quit() # fechar janela
-                    quit() # fechar jogo
+            
             
--- codegame/menu.py
+++ codegame/menu.py
@@ -1,9 +1,41 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from tkinter.font import Font
+import pygame
+
+from codegame.const import WIN_WIDTH
+
 class Menu:
     def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load('./assets/Battleground4.png')
+        self.rect = self.surf.get_rect(left=0,top=0)
+        
 
-    def run(self, ):
-        pass
+        
+    def run(self):
+        pygame.mixer.music.load('./assets/bgmusic.mp3')
+        pygame.mixer.music.play(loops=-1, start=1.5, fade_ms=0)
+
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(100, 'DEATH', (139, 0, 0), ((WIN_WIDTH/2), 100))
+            self.menu_text(100, 'SHOOTER', (139, 0, 0), ((WIN_WIDTH/2), 200))
+            pygame.display.flip()
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    print('Fechando janela')
+                    pygame.quit() # fechar janela
+                    quit() # fechar jogo
+
+        
+            pass
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font = pygame.font.Font("assets/fonts/Nosifer-Regular.ttf", text_size)
+        text_surf: pygame.Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: pygame.Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
+            
